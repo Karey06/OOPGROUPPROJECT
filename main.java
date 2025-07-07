de5adc8038275d2c79d89900306c03ffbd36163f
Joy Karanja <joyphenomenal@gmail.com>
2025-07-07 22:37:39 +0300

    Code for CartItem

---
+++ main.java
@@ -0,0 +1,6 @@
+public class main{
+    public static void main(String[] args) {
+        CartItem item = new CartItem("B001", "Java Programming", 1500.0, 2);
+        System.out.println(item);
+    }
+}
